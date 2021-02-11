c85b728a6622fa00ae8d8820b9e4ab3fe4b80cb3
lev <Angelina&25112011>
2021-02-11 10:10:28 +0200

    Added animation

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,79 @@
 <template>
-    <div id="app" class="container">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username"
-                   aria-describedby="button-addon2" v-model="hash">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="addToHash">Button</button>
-        </div>
-        <ul class="list-group">
-            <ListHashItem v-for="(hashItem, index) in listItems" :item="hashItem" :key="index"/>
-        </ul>
-
+  <div id="app" class="container">
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Type here..." aria-label="Recipient's username"
+             aria-describedby="button-addon2" v-model="hash" @keyup.13="addToHash(hash)">
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="addToHash(hash)">Add</button>
     </div>
+    <ul class="list-group">
+      <ListHashItem v-for="(hashItem, index) in listItems" :item="hashItem" :key="index" @removeItem="removeItem"/>
+    </ul>
+
+  </div>
 
 </template>
 
 <script>
 
-    import ListHashItem from "./components/ListHashItem";
-    export default {
-        name: 'App',
+import ListHashItem from "./components/ListHashItem";
 
-        mounted() {
-            window.location.hash = `${this.hashTitle}=red,blue,purple`
-            this.getListItemsFromHash(window.location.hash)
-        },
-        data(){
-            return{
-                listItems:[],
-                route: window.location.hash,
-                hash:'',
-                hashTitle: '#tags'
+export default {
+  name: 'App',
 
-            }
-        },
-        components: {ListHashItem},
-        methods:{
-            addToHash(){
-                console.log(this.hash)
-                window.location.hash +=`,${this.hash}`
-                console.log(window.location.hash)
-            },
-            getListItemsFromHash(hash){
-                let hashArray = hash
-                    .split("&")
-                    .map(v => v.split("="))
-                    .reduce( (pre, [key, value]) => ({ ...pre, [key]: value }), {} )
-                this.listItems = hashArray[this.hashTitle].split(",");
+  mounted() {
+    window.location.hash = `${this.hashTitle}=red,blue,purple`
+    this.getListItemsFromHash(window.location.hash)
+  },
+  data() {
+    return {
+      listItems: [],
+      route: window.location.hash,
+      hash: '',
+      hashTitle: '#tags'
 
-            }
-        },
-
-        watch: {
-            route: function(){
-                console.log(window.location.hash.split("#/")[1])
-            }
-        }
     }
+  },
+  components: {ListHashItem},
+  methods: {
+    addToHash(hash) {
+      if (hash.length > 0) {
+        this.listItems.push(hash)
+        window.location.hash = `${this.hashTitle}=${this.listItems.join(',')}`
+        this.hash =''
+      }
+    },
+    getListItemsFromHash(hash) {
+      let hashArray = hash
+          .split("&")
+          .map(v => v.split("="))
+          .reduce((pre, [key, value]) => ({...pre, [key]: value}), {})
+      this.listItems = hashArray[this.hashTitle].split(",");
+    },
+    removeItem(item){
+      this.listItems = this.listItems.filter(listItem=> item !== listItem)
+      window.location.hash = `${this.hashTitle}=${this.listItems.join(',')}`
+    }
+  },
+
+  watch: {
+    route: function () {
+      console.log(window.location.hash.split("#/")[1])
+    }
+  }
+}
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-        width: 30%;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 30%;
+  background-color: #eff2f1;
+  padding: 30px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
 </style>
--- src/components/ListHashItem.vue
+++ src/components/ListHashItem.vue
@@ -1,11 +1,13 @@
 <template>
-    <li class="list-group-item">{{ item }}</li>
+    <li class="list-group-item animate-li li-item" @click="$emit('removeItem', item)" title="Click to remove">{{ item }}</li>
 </template>
 
 <script>
+
     export default {
         name: "ListHashItem",
-        props:{
+
+      props:{
             item:{
                 type: String,
                 required: true
@@ -15,5 +17,24 @@
 </script>
 
 <style scoped>
+.li-item:hover{
+  background-color: #6c757d;
+  cursor: pointer;
+  color: #eff2f1;
+}
+.animate-li {
 
+  animation: trAnimate 1s linear;
+}
+@keyframes trAnimate {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
